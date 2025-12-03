6c08c9ed3db75a8a5392f0b596f76da7dab5c69f
Alex Zarin <vector-777@yandex.ru>
2025-12-03 16:28:10 +0300

    change test files

--- tests/ui/RealworldTest.spec.js
+++ tests/ui/RealworldTest.spec.js
@@ -1,4 +1,4 @@
-/*import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { App } from '../../src/pages/index';
 import { UserBuilder, ArticleBuilder } from '../../src/helpers/builders/index';
 
@@ -129,4 +129,4 @@ test.describe('Тесты для сайта realworld.qa', () => {
                 
         await expect(app.main.tag).toBeVisible();
     });
-});*/
+});
